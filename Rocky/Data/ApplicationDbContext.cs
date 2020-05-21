df116c1d17ac11b6eccd5eb3e446a1dc474c59d5
Ben Spark <bhrugen1990@gmail.com>
2020-05-21 13:13:42 -0500

    Section 2 - Complete DbContext Configuration

--- Rocky/Data/ApplicationDbContext.cs
+++ Rocky/Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Rocky.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,5 +14,6 @@ namespace Rocky.Data
 
         }
 
+        public DbSet<Category> Category { get; set; }
     }
 }
--- Rocky/Startup.cs
+++ Rocky/Startup.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Rocky.Data;
 
 namespace Rocky
 {
@@ -23,6 +25,10 @@ namespace Rocky
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(
+                Configuration.GetConnectionString("DefaultConnection")));
+
             services.AddControllersWithViews();
         }
 
