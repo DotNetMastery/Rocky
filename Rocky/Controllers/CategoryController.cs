6d95231fe8d4e061d6ec52c8f4b1f950f866dc7e
Ben Spark <bhrugen1990@gmail.com>
2020-05-21 16:39:48 -0500

    Section 3 - Category Edit Get Action

--- Rocky/Controllers/CategoryController.cs
+++ Rocky/Controllers/CategoryController.cs
@@ -46,5 +46,23 @@ namespace Rocky.Controllers
             return View(obj);
 
         }
+
+
+        //GET - EDIT
+        public IActionResult Edit(int? id)
+        {
+            if(id==null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Category.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
     }
 }
