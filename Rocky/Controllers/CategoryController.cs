ed401efa4911f10a192b720f84c7ca63864df96c
Ben Spark <bhrugen1990@gmail.com>
2020-05-21 16:21:46 -0500

    Section 3 - Validations in ASP.NET Core

--- Rocky/Controllers/CategoryController.cs
+++ Rocky/Controllers/CategoryController.cs
@@ -37,9 +37,14 @@ namespace Rocky.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Category obj)
         {
-            _db.Category.Add(obj);
-            _db.SaveChanges();
-            return RedirectToAction("Index");
+            if (ModelState.IsValid)
+            {
+                _db.Category.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+
         }
     }
 }
--- Rocky/Models/Category.cs
+++ Rocky/Models/Category.cs
@@ -11,8 +11,11 @@ namespace Rocky.Models
     {
         [Key]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         [DisplayName("Display Order")]
+        [Required]
+        [Range(1,int.MaxValue,ErrorMessage ="Display Order for category must be greater than 0")]
         public int DisplayOrder { get; set; }
     }
 }
