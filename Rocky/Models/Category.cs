11c600352e7f38becec7826d39076f386a37eaa2
Ben Spark <bhrugen1990@gmail.com>
2020-05-21 14:25:31 -0500

    Section 2 - Add Category Part 2

--- Rocky/Models/Category.cs
+++ Rocky/Models/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
@@ -11,6 +12,7 @@ namespace Rocky.Models
         [Key]
         public int Id { get; set; }
         public string Name { get; set; }
+        [DisplayName("Display Order")]
         public int DisplayOrder { get; set; }
     }
 }
