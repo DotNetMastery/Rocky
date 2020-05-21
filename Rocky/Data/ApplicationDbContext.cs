8eae289a559eeee8844006332dbc965cc069ede4
Ben Spark <bhrugen1990@gmail.com>
2020-05-21 13:08:20 -0500

    Section 2 - Setup DbContext

---
+++ Rocky/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rocky.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
