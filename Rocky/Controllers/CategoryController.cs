733602b18f53f93be3cf0ee3b820dfa9ecadd91e
Ben Spark <bhrugen1990@gmail.com>
2020-05-21 16:59:34 -0500

    Section 3 - Update Category

--- Rocky/Controllers/CategoryController.cs
+++ Rocky/Controllers/CategoryController.cs
@@ -64,5 +64,20 @@ namespace Rocky.Controllers
             return View(obj);
         }
 
+        //POST - EDIT
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Category.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+
+        }
+
     }
 }
