329ededbecb87b2c83ca55f0026115b62b96aa0f
Ben Spark <bhrugen1990@gmail.com>
2020-05-21 14:31:06 -0500

    Section 2 - Add Category Part 3

--- Rocky/Controllers/CategoryController.cs
+++ Rocky/Controllers/CategoryController.cs
@@ -30,5 +30,16 @@ namespace Rocky.Controllers
         {
             return View();
         }
+
+
+        //POST - CREATE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            _db.Category.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
     }
 }
