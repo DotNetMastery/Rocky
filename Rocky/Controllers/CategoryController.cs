c3fe90b7918ede0053831b2dd978f9bff043f825
Ben Spark <bhrugen1990@gmail.com>
2020-05-21 14:16:04 -0500

    Section 2 - Add Category Part 1

--- Rocky/Controllers/CategoryController.cs
+++ Rocky/Controllers/CategoryController.cs
@@ -23,5 +23,12 @@ namespace Rocky.Controllers
             IEnumerable<Category> objList = _db.Category;
             return View(objList);
         }
+
+
+        //GET - CREATE
+        public IActionResult Create()
+        {
+            return View();
+        }
     }
 }
