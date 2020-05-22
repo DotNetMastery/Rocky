5e11fcd1acb13bc3615d660d2ef3beccc0630bce
Ben Spark <bhrugen1990@gmail.com>
2020-05-21 22:24:10 -0500

    Section 4 - Product Upsert Get Action

--- Rocky/Controllers/ProductController.cs
+++ Rocky/Controllers/ProductController.cs
@@ -31,17 +31,31 @@ namespace Rocky.Controllers
         }
 
 
-        //GET - CREATE
-        public IActionResult Create()
+        //GET - UPSERT
+        public IActionResult Upsert(int? id)
         {
-            return View();
+            Product product = new Product();
+            if (id == null)
+            {
+                //this is for create
+                return View(product);
+            }
+            else
+            {
+                product = _db.Product.Find(id);
+                if (product == null)
+                {
+                    return NotFound();
+                }
+                return View(product);
+            }
         }
 
 
-        //POST - CREATE
+        //POST - UPSERT
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Create(Category obj)
+        public IActionResult Upsert(Category obj)
         {
             if (ModelState.IsValid)
             {
@@ -54,37 +68,7 @@ namespace Rocky.Controllers
         }
 
 
-        //GET - EDIT
-        public IActionResult Edit(int? id)
-        {
-            if(id==null || id == 0)
-            {
-                return NotFound();
-            }
-            var obj = _db.Category.Find(id);
-            if (obj == null)
-            {
-                return NotFound();
-            }
-
-            return View(obj);
-        }
-
-        //POST - EDIT
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Edit(Category obj)
-        {
-            if (ModelState.IsValid)
-            {
-                _db.Category.Update(obj);
-                _db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(obj);
-
-        }
-
+   
         //GET - DELETE
         public IActionResult Delete(int? id)
         {
