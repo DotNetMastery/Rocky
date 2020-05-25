42b1156c5ef2d156558ecb46ac33332aa38ff8c7
Ben Spark <bhrugen1990@gmail.com>
2020-05-25 01:14:29 -0500

    Section 5 - Remove from Cart Action

--- Rocky/Controllers/HomeController.cs
+++ Rocky/Controllers/HomeController.cs
@@ -78,6 +78,25 @@ namespace Rocky.Controllers
             return RedirectToAction(nameof(Index));
         }
 
+        public IActionResult RemoveFromCart(int id)
+        {
+            List<ShoppingCart> shoppingCartList = new List<ShoppingCart>();
+            if (HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart) != null
+                && HttpContext.Session.Get<IEnumerable<ShoppingCart>>(WC.SessionCart).Count() > 0)
+            {
+                shoppingCartList = HttpContext.Session.Get<List<ShoppingCart>>(WC.SessionCart);
+            }
+
+            var itemToRemove = shoppingCartList.SingleOrDefault(r => r.ProductId == id);
+            if (itemToRemove != null)
+            {
+                shoppingCartList.Remove(itemToRemove);
+            }
+            
+            HttpContext.Session.Set(WC.SessionCart, shoppingCartList);
+            return RedirectToAction(nameof(Index));
+        }
+
         public IActionResult Privacy()
         {
             return View();
