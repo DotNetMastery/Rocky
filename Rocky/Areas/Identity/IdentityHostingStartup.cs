2f304be18cf710cbd3bad50f1a2f3d6f4249fc39
Ben Spark <bhrugen1990@gmail.com>
2020-05-25 01:37:36 -0500

    Section 6 - Scaffold Identity Pages

---
+++ Rocky/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Rocky.Data;
+
+[assembly: HostingStartup(typeof(Rocky.Areas.Identity.IdentityHostingStartup))]
+namespace Rocky.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
--- Rocky/Startup.cs
+++ Rocky/Startup.cs
@@ -67,6 +67,7 @@ namespace Rocky
             app.UseSession();
             app.UseEndpoints(endpoints =>
             {
+                endpoints.MapRazorPages();
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
