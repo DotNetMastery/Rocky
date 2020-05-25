66fca2b6ee1a64df6e731abfda247f109cfff388
Ben Spark <bhrugen1990@gmail.com>
2020-05-25 01:50:01 -0500

    Section 6 - Add Name and Phone Number to Registeration

--- Rocky/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Rocky/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -13,6 +13,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
+using Rocky.Models;
 
 namespace Rocky.Areas.Identity.Pages.Account
 {
@@ -60,6 +61,9 @@ namespace Rocky.Areas.Identity.Pages.Account
             [Display(Name = "Confirm password")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
+
+            public string FullName { get; set; }
+            public string PhoneNumber { get; set; }
         }
 
         public async Task OnGetAsync(string returnUrl = null)
@@ -74,7 +78,8 @@ namespace Rocky.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = new IdentityUser { UserName = Input.Email, Email = Input.Email };
+                var user = new ApplicationUser { UserName = Input.Email, Email = Input.Email, PhoneNumber=Input.PhoneNumber,
+                FullName = Input.FullName};
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
--- Rocky/WC.cs
+++ Rocky/WC.cs
@@ -9,5 +9,8 @@ namespace Rocky
     {
         public static string ImagePath = @"\images\product\";
         public static string SessionCart = "ShoppingCartSession";
+
+        public static string AdminRole = "Admin";
+        public static string CustomerRole = "Customer";
     }
 }
