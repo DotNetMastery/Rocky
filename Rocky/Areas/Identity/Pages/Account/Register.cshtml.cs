2f725fe614780ba002b15487efb0da97f4bfa2d2
Ben Spark <bhrugen1990@gmail.com>
2020-05-25 02:15:16 -0500

    Section 6 - Create New Admin Users

--- Rocky/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Rocky/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -92,8 +92,16 @@ namespace Rocky.Areas.Identity.Pages.Account
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
-
-                    await _userManager.AddToRoleAsync(user, WC.AdminRole);
+                    if (User.IsInRole(WC.AdminRole))
+                    {
+                        //an admin has logged in and they try to create a new user
+                        await _userManager.AddToRoleAsync(user, WC.AdminRole);
+                    }
+                    else
+                    {
+                        await _userManager.AddToRoleAsync(user, WC.CustomerRole);
+                    }
+                    
                     _logger.LogInformation("User created a new account with password.");
 
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
