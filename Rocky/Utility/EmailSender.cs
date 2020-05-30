0805dcb4a7aa33587ff8bf04ee31e734a147fcd8
Ben Spark <bhrugen1990@gmail.com>
2020-05-30 16:07:23 -0500

    Section 7 - MailJet Implementation

---
+++ Rocky/Utility/EmailSender.cs
@@ -0,0 +1,62 @@
+﻿using Mailjet.Client;
+using Mailjet.Client.Resources;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rocky.Utility
+{
+    public class EmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            return Execute(email, subject, htmlMessage);
+        }
+
+        public async Task Execute(string email, string subject, string body)
+        {
+            MailjetClient client = new MailjetClient("1849179c639712b25373747961d13956", "069c8c5c0158d0d29c5a3ac900c9dae6")
+            {
+                Version = ApiVersion.V3_1,
+            };
+            MailjetRequest request = new MailjetRequest
+            {
+                Resource = Send.Resource,
+            }
+             .Property(Send.Messages, new JArray {
+     new JObject {
+      {
+       "From",
+       new JObject {
+        {"Email", "ben.spark90@yahoo.com"},
+        {"Name", "Ben"}
+       }
+      }, {
+       "To",
+       new JArray {
+        new JObject {
+         {
+          "Email",
+          email
+         }, {
+          "Name",
+          "DotNetMastery"
+         }
+        }
+       }
+      }, {
+       "Subject",
+       subject
+      }, {
+       "HTMLPart",
+       body
+      }
+     }
+             });
+            await client.PostAsync(request);
+        }
+    }
+}
