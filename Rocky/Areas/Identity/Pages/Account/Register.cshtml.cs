2fefd4bf3b65f020fe3f18d5961f3f2b403599cd
Ben Spark <bhrugen1990@gmail.com>
2020-06-10 17:15:21 -0500

    Section 7 - Register Admin User Bug

--- Rocky/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Rocky/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -121,7 +121,14 @@ namespace Rocky.Areas.Identity.Pages.Account
                     }
                     else
                     {
-                        await _signInManager.SignInAsync(user, isPersistent: false);
+                        if (!User.IsInRole(WC.AdminRole))
+                        {
+                            await _signInManager.SignInAsync(user, isPersistent: false);
+                        }
+                        else
+                        {
+                            return RedirectToAction("Index");
+                        }
                         return LocalRedirect(returnUrl);
                     }
                 }
